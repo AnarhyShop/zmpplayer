980bd778e0fa30ecaad9748ce84acaf9f65bf6db
AnarhyShop <vasiliykartoxa@gmail.com>
2026-06-14 19:49:43 +0300

    Update ura.lua

--- ura.lua
+++ ura.lua
@@ -1,17 +1,7 @@
 for i = 1, 27 do
-    shell.run("wget",
-        "https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/"..i..".dfpwm",
-        i..".dfpwm")
+    shell.run("wget https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/" .. i .. ".dfpwm " .. i .. ".dfpwm")
 end
 
-shell.run("wget",
-    "https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/play.lua",
-    "play.lua")
-
-shell.run("wget",
-    "https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/gui_play.lua",
-    "gui_play.lua")
-
-shell.run("wget",
-    "https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/make_disk.lua",
-    "make_disk.lua")
+shell.run("wget https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/play.lua play.lua")
+shell.run("wget https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/gui_play.lua gui_play.lua")
+shell.run("wget https://raw.githubusercontent.com/AnarhyShop/zmpplayer/main/make_disk.lua make_disk.lua")
